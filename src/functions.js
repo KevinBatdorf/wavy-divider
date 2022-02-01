4d959fe64224f9090daee6b564f4fe80692cab7c
Kevin Batdorf <kbat82@gmail.com>
2022-02-01 17:27:39 -0500

    fix spacing for firefox

--- src/functions.js
+++ src/functions.js
@@ -11,25 +11,34 @@ const random = (min, max) => {
 }
 
 const buildLines = ({ points, height, variations }) => {
-    return [...Array(points)].map((_, i) => {
-        const point = height - height * variations[i][0]
-        return ` L ${((i + 1) / points) * 100},${point}`
-    })
+    return [...Array(points)]
+        .map((_, i) => {
+            const point = height - height * variations[i][0]
+            return ` L ${(((i + 1) / points) * 100).toFixed(0)},${point.toFixed(
+                0,
+            )}`
+        })
+        .join('')
 }
 
 const buildCurves = ({ points, height, variations }) => {
     let previousX = 0
-    return [...Array(points)].map((_, i) => {
-        const start = height - height * variations[i][0]
-        const end = height - height * variations[i][1]
-        const smoothness = Math.max(10 / points, 0.1)
-        const spread = Math.min(points / 2, 5)
-        const x = ((i + 1) / points) * 100
-        const startX = Math.max(x - smoothness - spread, previousX)
-        const endX = Math.max(x + smoothness, previousX)
-        previousX = endX
-        return ` S ${startX},${start} ${Math.min(endX, 100)},${end}`
-    })
+    return [...Array(points)]
+        .map((_, i) => {
+            const start = height - height * variations[i][0]
+            const end = height - height * variations[i][1]
+            const smoothness = Math.max(10 / points, 0.1)
+            const spread = Math.min(points / 2, 5)
+            const x = ((i + 1) / points) * 100
+            const startX = Math.max(x - smoothness - spread, previousX)
+            const endX = Math.max(x + smoothness, previousX)
+            previousX = endX
+            return ` S ${startX.toFixed(0)},${start.toFixed(0)} ${Math.min(
+                endX.toFixed(0),
+                100,
+            )},${end.toFixed(0)}`
+        })
+        .join('')
 }
 
 export const buildPath = ({ height, points, direction, smoothness }) => {
@@ -43,7 +52,7 @@ export const buildPath = ({ height, points, direction, smoothness }) => {
 
         return [...acc, [from, end]]
     }, [])
-    const startingPosition = `M 0,${height - start}`
+    const startingPosition = `M 0,${(height - start).toFixed(0)}`
     const curves =
         points < 3 || smoothness === 'rigid'
             ? buildLines({ points, height, variations })
